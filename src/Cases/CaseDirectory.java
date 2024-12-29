b42a36e6400bf51870767379f1876c6a9c488500
h4ggstrom <robin2angelis@gmail.com>
2024-12-29 20:33:48 +0100

    fix: correct argument indexing for directory checks and command options in CaseDirectory

--- src/Cases/CaseDirectory.java
+++ src/Cases/CaseDirectory.java
@@ -23,30 +23,30 @@ public class CaseDirectory extends Case {
         }
 
         //case directory does not exists
-        File directory = new File(args[1]);
+        File directory = new File(args[2]);
         Repertoire R = new Repertoire(directory);
         if (!directory.exists() || !directory.isDirectory()) {
-            throw new Exception("Directory Does Not Exist");
+            throw new Exception("Directory " + args[2] +" Does Not Exist");
         }
         
 
         //case there is an option --stats or not
         if (args.length == 3) {
-            if (args[2].equals("--stat")) {
+            if (args[1].equals("--stat")) {
                 System.out.println(R.nbrImage() + "/n" + R.nbrImagesPNG() + "/n" + R.nbrImagesPNG() + "/n" + R.nbrImagesWEBP());
             } 
             else {
-                if(args[2].equals("--list")) {
+                if(args[1].equals("--list")) {
                     System.out.println("Les documents du répertoire " + args[1] + " :");
                     System.out.println(R);
                 }
                 else  {
-                    if (args[2].equals("--snapshotsave")) {
+                    if (args[1].equals("--snapshotsave")) {
                         Snapshot s = new Snapshot(args[1]);
                         s.saveSnapshot(args[1]);
                 } 
                     else {
-                        if(args[2].equals("--snapshotcompare")) {
+                        if(args[1].equals("--snapshotcompare")) {
                             Snapshot s = new Snapshot(args[1]);
                             Snapshot s2 = new Snapshot(args[4]);
                             File file1 = new File(args[1]);
