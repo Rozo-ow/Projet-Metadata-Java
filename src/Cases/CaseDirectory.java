dd8365114e43440ad60f34c14a9736aecfe044d8
Rozo-ow <enzo.de-angelis@etu.u-cergy.fr>
2025-01-09 11:13:28 +0100

    udpate cases

--- src/Cases/CaseDirectory.java
+++ src/Cases/CaseDirectory.java
@@ -23,7 +23,7 @@ public class CaseDirectory extends Case {
         }
 
         //case directory does not exists
-        File directory = new File(args[2]);
+        File directory = new File(args[1]);
         Repertoire R = new Repertoire(directory);
         if (!directory.exists() || !directory.isDirectory()) {
             throw new Exception("Directory " + args[2] +" Does Not Exist");
@@ -32,34 +32,29 @@ public class CaseDirectory extends Case {
 
         //case there is an option --stats or not
         if (args.length == 3) {
-            if (args[1].equals("--stat")) {
-                System.out.println(R.nbrImage() + "/n" + R.nbrImagesPNG() + "/n" + R.nbrImagesPNG() + "/n" + R.nbrImagesWEBP());
+            if (args[2].equals("--stat")){
+                System.out.println("nombre image " +R.nbrImage() + "\n" + "nombre PNG "+ R.nbrImagesPNG() + "\n" + "nombre JPEG " +R.nbrImagesJPEG() + "\n" + "nombre WEBP " +R.nbrImagesWEBP());
             } 
-            else {
-                if(args[1].equals("--list")) {
-                    System.out.println("Les documents du répertoire " + args[1] + " :");
-                    System.out.println(R);
-                }
-                else  {
-                    if (args[1].equals("--snapshotsave")) {
-                        Snapshot s = new Snapshot(args[1]);
-                        s.saveSnapshot(args[1]);
-                } 
-                    else {
-                        if(args[1].equals("--snapshotcompare")) {
-                            Snapshot s = new Snapshot(args[1]);
-                            Snapshot s2 = new Snapshot(args[4]);
-                            File file1 = new File(args[1]);
-                            // a reparer
-                            File file2 = new File(args[4]);
-                            System.out.println(Snapshot.compareSnapshot(file1, file2));
-                        }
-                        else {
-                            throw new Exception("Options Does Not Exist");
-                        }
-                    }
-                }
+            if(args[2].equals("--list")) {
+                System.out.println("Les documents du répertoire " + args[1] + " :");
+                System.out.println(R);
             }
         }
+        else if (args.length == 4) {
+                if (args[2].equals("--snapshotsave")) {
+                    Snapshot s = new Snapshot(args[1]);
+                    s.saveSnapshot(args[1]);
+                } 
+                if(args[2].equals("--snapshotcompare")) {
+                    Snapshot s = new Snapshot(args[1]);
+                    Snapshot s2 = new Snapshot(args[3]);
+                    File file1 = new File(args[1]);
+                    File file2 = new File(args[3]);
+                    System.out.println(Snapshot.compareSnapshot(file1, file2));
+                }
+        }
+        else {
+            throw new Exception("Options Does Not Exist");
+        }                    
     }
 }
--- src/Cases/CaseFile.java
+++ src/Cases/CaseFile.java
@@ -1,12 +1,11 @@
 package Cases;
-
-import java.io.File;
+import traitment.*;
 
 public class CaseFile extends Case {
 
     public void file(String[] args) throws Exception{
         //---------------------------------------------------------------------
-        //Cas d'erreurs
+        //checking if the file or filepath is provided
         //---------------------------------------------------------------------
         //checking if the file or filepath is provided
         if(args.length == 1)
@@ -15,12 +14,43 @@ public class CaseFile extends Case {
         }
 
         //checking if there is too much arguments
-        if(args.length > 5)
+        if(args.length > 4)
         {
             throw new Exception("Too much arguments Exception");
         }
+        System.out.println(args[2]);
 
-                //checking if the file exists
-       }
+        switch (args[2]) {
+            case "--stat":
+                MetadataExtractor s = new MetadataExtractor(args[1]);
+                System.out.println("titre : " +s.getTitle()+ "\n" + "taille : " + s.getFileSize()+ "\n" + "dimension : " + s.getDimensions());
+                break;
+            case "--info":
+                MetadataExtractor s1 = new MetadataExtractor(args[1]);
+                System.out.println("description : " + s1.getDescription() + "\n" + "geolocation : " + s1.getGeolocation()+ "\n" + "type MIME : " + s1.getMime());
+                if (args.length == 4){
+                    if(args[3]== "--stat"){
+                        System.out.println("titre : " +s1.getTitle()+ "\n" + "taille : " + s1.getFileSize()+ "\n" + "dimension : " + s1.getDimensions());
+                    }
+                }
+            default:
+                break;
+        }
+        /*
+        if(args[2]== "--stat"){
+            System.out.println("oui");
+            MetadataExtractor s = new MetadataExtractor(args[1]);
+            System.out.println("titre : " +s.getTitle()+ "/n" + "taille : " + s.getFileSize()+ "/n" + "dimension : " + s.getDimensions());
+        }
+        if(args[2]== "--info"){
+            MetadataExtractor s = new MetadataExtractor(args[1]);
+            System.out.println("description : " + s.getDescription() + "/n" + "geolocation : " + s.getGeolocation()+ "/n" + "type MIME : " + s.getMime());
+            if (args.length == 4){
+                if(args[3]== "--stat"){
+                    System.out.println("titre : " +s.getTitle()+ "/n" + "taille : " + s.getFileSize()+ "/n" + "dimension : " + s.getDimensions());
+                }
+            }
+        }*/
+    }
 }
         
